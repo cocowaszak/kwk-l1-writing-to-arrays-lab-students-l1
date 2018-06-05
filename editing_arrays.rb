6474dc84dc74939c042137ba9c14e8ef857d0b8b
cocowaszak <colettewaszak@gmail.com>
2018-06-05 22:24:20 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -11,6 +11,7 @@ def change_rainbow_colors
 end
 change_rainbow_colors
 def add_colors
- rainbow_colors = [  "yellow", "default", "light_cyan"]
+ rainbow_colors = [  "red", "light_red", "light_yellow"]
+ rainbow_colors.push
 end
  
